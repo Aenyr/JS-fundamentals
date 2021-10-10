b595a1ee79eb172642bc811f472dec65fbd8416b
Julian Bogdan <djulianbogdan@gmail.com>
2021-10-10 20:34:24 +0200

    My fourth task

--- 01-ForLoops/04-ArrayAsStrings.js
+++ 01-ForLoops/04-ArrayAsStrings.js
@@ -2,7 +2,7 @@
 
 let data = ["1", "A", "B", "c", "r", true, NaN, undefined];
 
-console.log(data.join(""));
-// console.log(
-//   data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7]
-// );
+console.log(
+  data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7]
+);
+// console.log(data.join(""));
