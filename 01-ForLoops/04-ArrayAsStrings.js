f401aef7b138f35f2cfe09a96ebba4b56bd396cc
Julian Bogdan <djulianbogdan@gmail.com>
2021-10-10 17:32:47 +0200

    My fourth task

---
+++ 01-ForLoops/04-ArrayAsStrings.js
@@ -0,0 +1,4 @@
+//Write a program which prints the elements of the following array as a single string.
+
+var x = ["1", "A", "B", "c", "r", true, NaN, undefined];
+console.log(x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6] + x[7]);
