f54f26f4ea2dd6d78ca7a5ba25895996983a7cc2
Julian Bogdan <djulianbogdan@gmail.com>
2021-10-10 17:05:35 +0200

    My second task

--- 01-ForLoops/01-OddOrEven.js
+++ 01-ForLoops/01-OddOrEven.js
@@ -4,7 +4,6 @@
 function oddOrEven() {
   for (let i = 0; i < 15; i++) {
     if (i % 2 === 0) {
-      //console.log(i + " even");
       console.log(`${i} even`);
     } else {
       console.log(`${i} odd`);
