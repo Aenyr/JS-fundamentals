6cc2643672852cfc97eaaa8d6ff71178e9b637d5
Julian Bogdan <djulianbogdan@gmail.com>
2021-10-14 05:34:15 +0200

    Functions homework

--- 02-Functions/10-CertainLetterInString.js
+++ 02-Functions/10-CertainLetterInString.js
@@ -1,12 +1,12 @@
 // Write a program that calculates the number of appearances of a letter a in a given string.
 // Modify the program so it calculates the number of both letters a and A.
 
-function findLetter(sentance) {
+function findLetter(sentance, letter1, letter2) {
   let counter = 0;
-
-  if ("a" == sentance[i]) {
-    counter++;
+  for (let i = 0; i < sentance.length; i++) {
+    if (letter1 === sentance[i]) counter++;
+    if (letter2 === sentance[i]) counter++;
   }
   return counter;
 }
-console.log(findLetter("come what may"));
+console.log(findLetter("for each And every one", "a", "A"));
